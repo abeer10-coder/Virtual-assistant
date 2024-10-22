dc4533f6c627420568582a49b9d580cbe628a325
abeer10-coder <saptarshiabeerwork2004@gmail.com>
2024-10-22 12:46:18 +0530

    Add files via upload

---
+++ Music_library.py
@@ -0,0 +1,5 @@
+music={
+    'akhiyan gulab':'https://youtu.be/f1h6NZYem0o?si=G2dHVnYn_zQd-Grd',
+    'nadaniya':'https://youtu.be/gPpQNzQP6gE?si=svt5CL2iWSH6Pc8t',
+    'believer': 'https://youtu.be/7wtfhZwyrcc?si=zTEwPiTW2r7GQJSq'
+}
